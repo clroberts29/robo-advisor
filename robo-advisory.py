04aa6262fe3b0c4da15bd131b3917750d28adb34
clroberts29 <50728923+clroberts29@users.noreply.github.com>
2019-06-21 19:30:47 -0400

    last refreshed added

--- robo-advisory.py
+++ robo-advisory.py
@@ -7,21 +7,17 @@ import json
 request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=demo"
 
 response = requests.get(request_url)
-print(type(response))
-print(response.status_code)
-print(response.text)
+# print(type(response))
+# print(response.status_code)
+# print(response.text)
 
 dict_response = json.loads(response.text)
 
-quit()
+last_refreshed = dict_response["Meta Data"]["3. Last Refreshed"]
+
 
 
 # INFO OUTPUTS
-
-
-
-
-
 
 print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
@@ -29,7 +25,7 @@ print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
 print("REQUEST AT: 2018-02-20 02:00pm")
 print("-------------------------")
-print("LATEST DAY: 2018-02-20")
+print("LATEST DAY: " + last_refreshed)
 print("LATEST CLOSE: $100,000.00")
 print("RECENT HIGH: $101,000.00")
 print("RECENT LOW: $99,000.00")
